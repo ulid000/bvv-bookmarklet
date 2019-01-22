c13c210fff59461c514dd06cd2eb27698a4ba127
Uli Deiters <ulrich.deiters@web.de>
2019-01-22 23:31:20 +0100

    moved code to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-import bookmarklet from '../dist_bookmarklet/bvvbookmarklet.bookmarklet';
-var a = document.createElement('a');
-a.href = bookmarklet;
-a.innerHTML = "Install Bookmarklet!";
-document.body.appendChild(a);
+import bvvbookmarklet from '../dist_bookmarklet/bvvbookmarklet.bookmarklet';
+
+// based on https://gist.github.com/Chalarangelo/4ff1e8c0ec03d9294628efbae49216db
+// and https://stackoverflow.com/a/34046084
+// TODO: differ between iOS and Android if needed
+const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.readonly = false; // needed on iOS 
+    el.contenteditable = true; // needed on iOS    
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    range = document.createRange();
+    range.selectNodeContents(el);
+    var s = window.getSelection();
+    s.removeAllRanges();
+    s.addRange(range);
+    el.setSelectionRange(0, 999999);
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
+
+var bookmarkletcopy = document.getElementById('bookmarkletcopy');
+bookmarkletcopy.href = "javascript:(copyToClipboard(" + bookmarklet + "))";
+
+var bookmarkletlink = document.getElementById('bookmarkletlink')
+bookmarkletlink.href = bvvbookmarklet;
