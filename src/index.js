b458eafbbc25b53fc25e9c81ae8087d71dae941d
Uli Deiters <ulrich.deiters@web.de>
2019-03-21 23:55:58 +0100

    differing between iOS and Android in copy

--- src/index.js
+++ src/index.js
@@ -2,31 +2,37 @@ import bvvbookmarklet from '../dist_bookmarklet/bvvbookmarklet.bookmarklet';
 
 // based on https://gist.github.com/Chalarangelo/4ff1e8c0ec03d9294628efbae49216db
 // and https://stackoverflow.com/a/34046084
-// TODO: differ between iOS and Android if needed
+// and https://gist.github.com/rproenca/64781c6a1329b48a455b645d361a9aa3
 export function copyToClipboard(str) {
     var el = document.createElement('textarea');
     el.value = str;
-    el.readonly = false; // needed on iOS 
-    el.contenteditable = true; // needed on iOS    
     el.style.position = 'absolute';
     el.style.left = '-9999px';
     document.body.appendChild(el);
-    var range = document.createRange();
-    range.selectNodeContents(el);
-    console.log(el.value);
-    console.log(range);
-    var s = window.getSelection();
-    s.removeAllRanges();
-    s.addRange(range);
-    el.setSelectionRange(0, 999999);
-    console.log(s);
+
+    if (navigator.userAgent.match(/ipad|iphone/i)) {
+        el.readonly = false; // needed on iOS 
+        el.contenteditable = true; // needed on iOS    
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        // console.log(el.value);
+        // console.log(range);
+        var s = window.getSelection();
+        s.removeAllRanges();
+        s.addRange(range);
+        el.setSelectionRange(0, 999999);
+        // console.log(s);
+    } else {
+        el.select();
+    }
+
     try {
         var successful = document.execCommand('copy');
         var msg = successful ? 'successful' : 'unsuccessful';
         console.log('Copying text command was ' + msg);
-      } catch(err) {
+    } catch (err) {
         console.log('Oops, unable to copy ' + err);
-      }
+    }
     document.body.removeChild(el);
 };
 
